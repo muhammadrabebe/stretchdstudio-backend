060e74579fd69fe65cafd78dde2c6b6b2c0d7d8e
Mohamed.Rabebe <Mohamed.Rabebe@SNS-RABEBE>
2025-02-06 21:00:21 +0200

    first commit

---
+++ src/main/java/com/strechdstudio/app/StrechdstudioApplication.java
@@ -0,0 +1,13 @@
+package com.strechdstudio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrechdstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrechdstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strechdstudio/app/StrechdstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strechdstudio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrechdstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
